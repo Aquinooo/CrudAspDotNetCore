c346ffb3632a6f07dd7739ea432698e240e7049f
Danilo Aquino <danilokronan@gmail.com>
2024-10-10 01:17:29 -0300

    Add project files.

---
+++ Crud/Models/BancoDeDados.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace Crud.Models
+{
+    public class BancoDeDados : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=Crud;Integrated Security=True");
+        }
+    }
+}
---
+++ Crud/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Crud.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Cpf { get; set; }
+
+        public string Telefone { get; set; }
+    }
+}
